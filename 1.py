aa730686cbf51645ce47d7999ff570114bf2e049
Ilya <yakubenko@complexis.biz>
2023-05-30 16:04:20 +0300

    task1 commit

---
+++ 1.py
@@ -0,0 +1,20 @@
+class restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    def describe_restaurant(self):
+        print("Название ресторана: ",self.restaurant_name,"\nТип кухни: ", self.cuisine_type, "\n")
+
+
+class IceCreamStand(restaurant):
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ["шоколадное", "фисташковое"]
+    def print_flavors(self):
+        print("Вкусы мороженого:")
+        for flavor in self.flavors:
+            print(flavor)
+
+stand = IceCreamStand("Мороженое", "Кафе")
+stand.describe_restaurant()
+stand.print_flavors()
